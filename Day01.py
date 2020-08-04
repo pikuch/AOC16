2854ffbedd6678b493eb72156fcfa065febe9d70
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-04 21:52:55 +0200

    day 1 finished

--- Day01.py
+++ Day01.py
@@ -12,40 +12,59 @@ class Tracker:
     def __init__(self, x, y, d):
         self.x = x
         self.y = y
-        self.d = d # 0 = north
-    def distance_to_zero(self):
+        self.d = d  # 0 = north
+        self.visited = [(x, y)]
+        self.revisited = []
+
+    def distance_from_zero(self):
         return abs(self.x) + abs(self.y)
+
+    def distance_from_zero_first_visited(self):
+        return abs(self.revisited[0][0]) + abs(self.revisited[0][1])
+
+    def check_revisited(self):
+        if len(self.revisited) == 0:
+            if (self.x, self.y) in self.visited:
+                self.revisited.append((self.x, self.y))
+            else:
+                self.visited.append((self.x, self.y))
+
     def move(self, where):
-
-
-# find a new position
-def decode_item(instruction, d, x, y, visited):
-    get_L = {"N": "W", "W": "S", "S": "E", "E": "N"}
-    get_R = {"W": "N", "N": "E", "E": "S", "S": "W"}
-    what_steps = {"N": [0, 1], "W": [-1, 0], "S": [0, -1], "E": [1, 0]}
-
-    if instruction[0] == "L":
-        d = get_L[d]
-    elif instruction[0] == "R":
-        d = get_R[d]
-    else:
-        print(f"Illegal instruction encountered: {instruction}")
-        exit(-1)
-
-    steps = int(instruction[1:])
-
-    for s in range
-
-    x += steps * what_steps[d][0]
-    y += steps * what_steps[d][1]
-
-    return d, x, y
+        # change direction
+        if where[0] == "L":
+            self.d = (self.d + 3) % 4
+        elif where[0] == "R":
+            self.d = (self.d + 1) % 4
+        else:
+            pass  # shouldn't happen, we can go straight
+        # walk
+        steps = int(where[1:])
+        if self.d == 0:
+            for i in range(steps):
+                self.y += 1
+                self.check_revisited()
+        elif self.d == 1:
+            for i in range(steps):
+                self.x += 1
+                self.check_revisited()
+        elif self.d == 2:
+            for i in range(steps):
+                self.y -= 1
+                self.check_revisited()
+        elif self.d == 3:
+            for i in range(steps):
+                self.x -= 1
+                self.check_revisited()
+        else:
+            pass  # shouldn't happen, we stay where we are
 
 
 # decode the path
-
+tracker = Tracker(0, 0, 0)
 for item in data:
-    direction, x, y = decode_item(item, direction, x, y, visited)
+    tracker.move(item)
 
 # print the answer
-print(f"The target is at ({x}, {y}) which is {abs(x)+abs(y)} blocks away.")
+print(f"The target is at ({tracker.x}, {tracker.y}) which is {tracker.distance_from_zero()} blocks away.")
+print(f"The first revisited position is at ({tracker.revisited[0][0]}, {tracker.revisited[0][1]})"
+      f" which is {tracker.distance_from_zero_first_visited()} blocks away.")
