d7d680f4872a19dd285b6b2083371d1ebfeaf4e0
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-04 16:41:03 +0200

    day 1 part 1 done

--- Day01.py
+++ Day01.py
@@ -1,2 +1,38 @@
+# load the data
 with open("Day01input.txt") as f:
     data = f.read()
+
+# data cleanup
+data = data.replace(' ', '')
+data = data.split(',')
+
+
+# find a new position
+def decode_item(instruction, d, x, y):
+    get_L = {"N": "W", "W": "S", "S": "E", "E": "N"}
+    get_R = {"W": "N", "N": "E", "E": "S", "S": "W"}
+    steps = {"N": [0, 1], "W": [-1, 0], "S": [0, -1], "E": [1, 0]}
+
+    if instruction[0] == "L":
+        d = get_L[d]
+    elif instruction[0] == "R":
+        d = get_R[d]
+    else:
+        print(f"Illegal instruction encountered: {instruction}")
+        exit(-1)
+
+    x += int(instruction[1:]) * steps[d][0]
+    y += int(instruction[1:]) * steps[d][1]
+
+    return d, x, y
+
+
+# decode the path
+direction = "N"
+x, y = 0, 0
+
+for item in data:
+    direction, x, y = decode_item(item, direction, x, y)
+
+# print the answer
+print(f"The target is at ({x}, {y}) which is {abs(x)+abs(y)} blocks away.")
