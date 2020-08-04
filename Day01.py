5bccf8485a4e8e6de9e1b839b52485243cd5e0a8
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-04 20:51:45 +0200

    class Tracker added

--- Day01.py
+++ Day01.py
@@ -7,11 +7,22 @@ data = data.replace(' ', '')
 data = data.split(',')
 
 
+# position tracker
+class Tracker:
+    def __init__(self, x, y, d):
+        self.x = x
+        self.y = y
+        self.d = d # 0 = north
+    def distance_to_zero(self):
+        return abs(self.x) + abs(self.y)
+    def move(self, where):
+
+
 # find a new position
-def decode_item(instruction, d, x, y):
+def decode_item(instruction, d, x, y, visited):
     get_L = {"N": "W", "W": "S", "S": "E", "E": "N"}
     get_R = {"W": "N", "N": "E", "E": "S", "S": "W"}
-    steps = {"N": [0, 1], "W": [-1, 0], "S": [0, -1], "E": [1, 0]}
+    what_steps = {"N": [0, 1], "W": [-1, 0], "S": [0, -1], "E": [1, 0]}
 
     if instruction[0] == "L":
         d = get_L[d]
@@ -21,18 +32,20 @@ def decode_item(instruction, d, x, y):
         print(f"Illegal instruction encountered: {instruction}")
         exit(-1)
 
-    x += int(instruction[1:]) * steps[d][0]
-    y += int(instruction[1:]) * steps[d][1]
+    steps = int(instruction[1:])
+
+    for s in range
+
+    x += steps * what_steps[d][0]
+    y += steps * what_steps[d][1]
 
     return d, x, y
 
 
 # decode the path
-direction = "N"
-x, y = 0, 0
 
 for item in data:
-    direction, x, y = decode_item(item, direction, x, y)
+    direction, x, y = decode_item(item, direction, x, y, visited)
 
 # print the answer
 print(f"The target is at ({x}, {y}) which is {abs(x)+abs(y)} blocks away.")
