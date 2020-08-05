d943cc5e46277a1345ff595833d7e6100c23048d
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-05 16:35:06 +0200

    day 6 done

--- Day06.py
+++ Day06.py
@@ -4,7 +4,7 @@ with open("Day06input.txt") as f:
     data = f.read().split("\n")
 
 
-def decode(data):
+def decode_high(data):
     msg = []
     for i in range(len(data[0])):
         chars = {}
@@ -18,4 +18,19 @@ def decode(data):
     return "".join(msg)
 
 
-print(decode(data))
+def decode_low(data):
+    msg = []
+    for i in range(len(data[0])):
+        chars = {}
+        for word in data:
+            if word[i] in chars:
+                chars[word[i]] += 1
+            else:
+                chars[word[i]] = 1
+        msg.append([k for k, v in sorted(chars.items(), key=lambda item: item[1])][0])
+
+    return "".join(msg)
+
+
+print(f"The message from the most frequent characters is {decode_high(data)}")
+print(f"The message from the least frequent characters is {decode_low(data)}")
