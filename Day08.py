b41d873ebde07bbf96bae2972575ee56ca26998b
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-06 08:48:08 +0200

    day 8 done

--- Day08.py
+++ Day08.py
@@ -19,6 +19,13 @@ class Screen:
                     print("░░", end="")
             print()
 
+    def count_lit(self):
+        counter = 0
+        for h in range(self.height):
+            for w in range(self.width):
+                counter += self.pixels[h][w]
+        return counter
+
     def run_cmd(self, cmd):
         words = cmd.split()
         if words[0] == "rect":
@@ -30,16 +37,23 @@ class Screen:
             if words[1] == "row":
                 place = int(words[2][2:])
                 shift = int(words[4])
-                for _ in range(shift):
-
-
+                new_row = []
+                for i in range(self.width):
+                    new_row.append((self.pixels[place][(i-shift+self.width) % self.width]))
+                self.pixels[place] = new_row
             elif words[1] == "column":
                 place = int(words[2][2:])
                 shift = int(words[4])
+                old_column = []
+                for i in range(self.height):
+                    old_column.append(self.pixels[i][place])
+                for i in range(self.height):
+                    self.pixels[i][place] = old_column[(i-shift+self.height) % self.height]
             else:
                 print(f"Detected an error in instructions: {words[1]}")
         else:
             print(f"Detected an error in instructions: {words[0]}")
+
 
 # load the data
 with open("Day08input.txt") as f:
@@ -49,5 +63,7 @@ screen = Screen()
 
 for d in data:
     screen.run_cmd(d)
-    screen.show()
 
+print(f"Number of lit pixels: {screen.count_lit()}")
+
+screen.show()
