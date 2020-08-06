98d6869db6221b7de1490d440e225e6d4a7e2520
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-06 09:40:16 +0200

    day 9 done

--- Day09.py
+++ Day09.py
@@ -24,8 +24,27 @@ def decompress(s):
     return result
 
 
+# get decompressed length (ver.2)
+def length_ver_2(s):
+    result = 0
+    pos = 0
+    while pos < len(s):
+        if s[pos] != "(":
+            result += 1
+            pos += 1
+        else:
+            closing_pos = s[pos:].find(")")
+            marker = s[pos+1:pos+closing_pos]
+            nchars, reps = decode(marker)
+            pos = pos + closing_pos + 1
+            result += length_ver_2(s[pos:pos+nchars]) * reps
+            pos += nchars
+    return result
+
+
 # load the data
 with open("Day09input.txt") as f:
     data = f.read()
 
 print(f"The decompressed length is {len(decompress(data))}")
+print(f"The decompressed length with version 2 is {length_ver_2(data)}")
