bc4736aba8d3333002794eacb3d271f025ddd833
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-19 15:23:37 +0200

    added time measurement

--- Day12.py
+++ Day12.py
@@ -1,4 +1,5 @@
 from BunnyComp import BunnyComp
+from time import time_ns
 
 # load the data
 with open("Day12input.txt") as f:
@@ -6,13 +7,19 @@ with open("Day12input.txt") as f:
 
 comp = BunnyComp()
 comp.load(data)
+t0 = time_ns()
 comp.run()
+td = time_ns() - t0
 
-print(f"Value in register a: {comp.reg['a']}")
+print(f"Time taken: {td//(10**6):_d} ms")
+print(f"Result 1 found: {comp.reg['a']}")
 
 comp = BunnyComp()
 comp.load(data)
 comp.reg['c'] = 1
+t0 = time_ns()
 comp.run()
+td = time_ns() - t0
 
-print(f"The correct value in register a: {comp.reg['a']}")
+print(f"Time taken: {td//(10**6):_d} ms")
+print(f"Result 2 found: {comp.reg['a']}")
