105b71693db6537ceab50e0ab96880f07bd40669
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-24 12:12:55 +0200

    day 22 done

--- Day22.py
+++ Day22.py
@@ -16,15 +16,38 @@ class Node:
 with open("Day22input.txt") as f:
     data = f.read().split("\n")
 
-nodes = []
+nodes = {}
 for item in data[2:]:
-    nodes.append(Node(item))
+    n = Node(item)
+    nodes[(n.x, n.y)] = n
 
 viable_count = 0
-for node1 in nodes:
-    for node2 in nodes:
+for node1 in nodes.values():
+    for node2 in nodes.values():
         if node1 != node2 and node1.used > 0 and node1.used <= node2.avail:
                 viable_count += 1
 
 print(f"Number of viable pairs: {viable_count}")
 
+# part 2
+
+# find the dimensions of the grid
+max_x, max_y = 0, 0
+for node in nodes.keys():
+    if node[0] > max_x:
+        max_x = node[0]
+    if node[1] > max_y:
+        max_y = node[1]
+
+# show the grid
+for y in range(max_y + 1):
+    for x in range(max_x + 1):
+        if nodes[(x, y)].used > 100:
+            print(" X ", end="")
+        elif nodes[(x, y)].used == 0:
+            print("   ", end="")
+        else:
+            print(" . ", end="")
+    print("")
+
+print("35 + 5 * 33 = 200 (calculated by hand)")
